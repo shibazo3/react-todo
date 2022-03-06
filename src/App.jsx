5574e1e0bac5a8bc7f3d4ba52d147cf137cf6e71
Tsubasa Shibata <33864413+shibazo3@users.noreply.github.com>
2022-03-06 20:18:12 +0900

    add validation

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTOODOは5個までです。消化しましょう。
+        </p>
+      )}
       <IncompleteTodos
         todo={incompleteTodos}
         onClickComplete={onClickComplete}
