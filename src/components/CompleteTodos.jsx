a6e7598da74c5084a7c0a7d90270e621b71ee17b
Tsubasa Shibata <33864413+shibazo3@users.noreply.github.com>
2022-03-06 20:11:12 +0900

    css-in-js

--- src/components/CompleteTodos.jsx
+++ src/components/CompleteTodos.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  margin: "8px",
+  backgroundColor: "#ffffe0",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  borderRadius: "8px"
+};
+
 export const CompleteTodos = (props) => {
   const { todo, onClickBack } = props;
   return (
-    <div className="complete-area">
+    <div style={style}>
       <p className="title">完了のTODO</p>
       <ul className="todo-list">
         {todo.map((todo, index) => {
--- src/components/IncompleteTodos.jsx
+++ src/components/IncompleteTodos.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c6ffe2",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "28px"
+};
+
 export const IncompleteTodos = (props) => {
   const { todo, onClickComplete, onClickDelete } = props;
   return (
-    <div className="incomplete-area">
+    <div style={style}>
       <p className="title">未完了のTODO</p>
-      <ul className="todo-lissst">
+      <ul className="todo-list">
         {todo.map((todo, index) => {
           return (
             <li key={todo}>
